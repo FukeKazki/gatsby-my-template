3df92b261b75799c596c86b36d06e30678b3f0c6
FukeKazki <kazkichi0906@gmail.com>
2022-04-19 08:28:44 +0900

    templateの作成

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby-my-template",
   },
-  plugins: ["gatsby-plugin-emotion"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: `gatsby-plugin-typegen`,
+      options: {
+          outputPath: `src/__generated__/gatsby-types.d.ts`
+      }
+  },
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+  "gatsby-transformer-sharp",
+  ],
 };
